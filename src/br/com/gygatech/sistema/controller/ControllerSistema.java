53b4258495c6e512c718c9b248de287f6199f10b
fabio <fabio@Dev-Server.gygatech.com.br>
2016-10-20 19:18:51 -0200

    Email Agenda

--- src/br/com/gygatech/sistema/controller/ControllerSistema.java
+++ src/br/com/gygatech/sistema/controller/ControllerSistema.java
@@ -3,15 +3,26 @@ package br.com.gygatech.sistema.controller;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.mail.EmailException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.gygatech.sistema.dao.AgendaDAO;
 import br.com.gygatech.sistema.dao.UsuarioDAO;
+import br.com.gygatech.sistema.mail.EmailAgenda;
+import br.com.gygatech.sistema.model.Agenda;
 import br.com.gygatech.sistema.model.Usuario;
 
 @Transactional
@@ -19,9 +30,12 @@ import br.com.gygatech.sistema.model.Usuario;
 public class ControllerSistema {
 	
 	final private String salt="Jh3gt4y57sfGtrc22";
-
+	
 	@Autowired
 	UsuarioDAO usuarioDao;
+	
+	@Autowired
+	AgendaDAO agendaDao;
 	
 	@RequestMapping("/login")
 	public String login(Usuario usuario, HttpSession session){
@@ -67,5 +81,34 @@ public class ControllerSistema {
 			e.printStackTrace();
 		}
 		return senha;
+	}
+	
+	@Scheduled(cron="0 15 19 * * *")
+	public void email(){
+		LocalDate hoje = LocalDate.now();
+		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String data = hoje.format(formatador);
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			cal.setTime(sdf.parse(data));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		List<Agenda> lista = agendaDao.consultaPorData(cal);
+		EmailAgenda email = new EmailAgenda();
+		if(lista.isEmpty()){
+			try {
+				email.enviaEmailVazio();
+			} catch (EmailException e) {
+				e.printStackTrace();
+			}
+		}else{
+			try {
+				email.enviaEmailAgenda(lista);
+			} catch (EmailException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- src/br/com/gygatech/sistema/mail/EmailAgenda.java
+++ src/br/com/gygatech/sistema/mail/EmailAgenda.java
@@ -1,24 +1,71 @@
 package br.com.gygatech.sistema.mail;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
 
+import br.com.gygatech.sistema.model.Agenda;
+
 public class EmailAgenda {
 
-	public void enviaEmailSimples() throws EmailException {
+	public void enviaEmailAgenda(List<Agenda> lista) throws EmailException {
+		
 		LocalDate hoje = LocalDate.now();
+		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String data = hoje.format(formatador);
+		
 		SimpleEmail email = new SimpleEmail();
 		email.setHostName("smtp.gmail.com"); // o servidor SMTP para envio do e‐mail
 		email.addTo("fabssilveira@gygatech.com.br"); //destinatário
 		email.setFrom("gygatech@gygatech.com.br"); // remetente
-		email.setSubject("Contato: "+hoje); // assunto do e‐mail
-		email.setMsg(""); //conteudo do e‐mail
+		email.setSubject("Sistema Gygatech: Agenda de "+data); // assunto do e‐mail
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("Atendimentos agendados para hoje:");
+		sb.append("\n");
+		int i = 1;
+		for(Agenda a: lista){
+			sb.append(i+"º: "+a.getHorario()+" - "+a.getCliente().getNome()+" - "+a.getInformacoes());
+			sb.append("\n");
+			i++;
+		}
+		sb.append("Gerado automaticamente pelo Sistema de Chamados Gygatech. Por favor não responda este email.");
+		
+		email.setMsg(sb.toString()); //conteudo do e‐mail
 		email.setAuthenticator(new DefaultAuthenticator("gygatech@gygatech.com.br", "k6a2q6"));
 		email.setSmtpPort(587);
 		email.setStartTLSEnabled(true);
 		email.send();
-		}
+	}
 
+	public void enviaEmailVazio() throws EmailException {
+		
+		LocalDate hoje = LocalDate.now();
+		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String data = hoje.format(formatador);
+		
+		SimpleEmail email = new SimpleEmail();
+		email.setHostName("smtp.gmail.com"); // o servidor SMTP para envio do e‐mail
+		email.addTo("fabssilveira@gygatech.com.br"); //destinatário
+		email.setFrom("gygatech@gygatech.com.br"); // remetente
+		email.setSubject("Sistema Gygatech: Agenda de "+data); // assunto do e‐mail
+		
+		StringBuilder sb1 = new StringBuilder();
+		sb1.append("Atendimentos agendados para hoje:");
+		sb1.append("\n");
+		sb1.append("Não há atendimentos agendados para hoje.");
+		sb1.append("\n");
+		sb1.append("Gerado automaticamente pelo Sistema de Chamados Gygatech. Por favor não responda este email.");
+		
+		email.setMsg(sb1.toString()); //conteudo do e‐mail
+		email.setAuthenticator(new DefaultAuthenticator("gygatech@gygatech.com.br", "k6a2q6"));
+		email.setSmtpPort(587);
+		email.setStartTLSEnabled(true);
+		email.send();
+	}
+	
 }
